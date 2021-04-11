55b0f5c9d3438454aec51c1cf6904f8a92710dbc
Ernst Vorsteveld <ernst.vorsteveld@gmail.com>
2021-04-11 09:32:22 +0200

    split lines

--- tests/mongo/test_mongo_connection.py
+++ tests/mongo/test_mongo_connection.py
@@ -11,6 +11,7 @@ class TestMongoConnection(unittest.TestCase):
         self.assertEqual(collection.full_name, "default_database_name.default_collection")
 
     def test_should_get_collection_from_parameter(self):
-        self.mongo = MongoConnection(FileProperties(os.path.dirname(os.path.realpath(__file__)) + "/mongo-connection-test.json"))
+        configFile = os.path.dirname(os.path.realpath(__file__)) + "/mongo-connection-test.json"
+        self.mongo = MongoConnection(FileProperties(configFile))
         collection = self.mongo.getCollection()
         self.assertEqual(collection.full_name, "test_database.test_collection")
